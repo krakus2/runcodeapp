0673737b8cfbe2cbec65ab93c51cc8abac2bdc67
filip.krakowiak <filip.krakowiak@gmail.com>
2018-10-30 10:14:25 +0100

    prepared for deploy

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ require('dotenv').config();
 //const debug = require('debug')('app:startup');
 const app = express();
 const winston = require('winston');
+const path = require('path');
 
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+    //for any route that isn't hit with routes/api use this route
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-   console.log(`Port is listening on ${port}`);
-   winston.info(`Port is listening on ${port}`);
+    console.log(`Port is listening on ${port}`);
+    winston.info(`Port is listening on ${port}`);
 });
